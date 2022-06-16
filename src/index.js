ae5d0eff8f9bf671d37da9f8b37b83b2e62f803d
nila_wilda <nilawildaalaluf@gmail.com>
2022-06-16 14:41:39 +0700

    testing

---
+++ src/index.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+const router = express.Router();
+// const PORT = process.env.PORT || 5050;
+const { books } = require("../handlers/books");
+
+router.get("/books", books);
+
+router.get("/", (req, res) => {
+  res.send("This is my demo project");
+});
+
+app.use("/.netlify/functions/index", router);
+
+module.exports.handler = serverless(app);
+
+// app.listen(PORT, function () {
+//   console.log(`Demo project at: ${PORT}!`);
+// });
